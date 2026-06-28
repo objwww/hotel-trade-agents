5d1dfa3652ca3b97343253c9868b1863d13ffda0
wangping <1369755002@qq.com>
2026-06-28 22:23:23 +0800

    init hotel trade agents project

---
+++ src/main/java/com/hotel/agents/HotelTradeAgentsApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.agents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelTradeAgentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelTradeAgentsApplication.class, args);
+    }
+
+}
